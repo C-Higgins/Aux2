d17eb4a3b1dd7760298a250ddc7483e5b142700a
Chris <vc.chris.higgins@lowes.com>
2017-06-27 13:41:54 -0400

    setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+
+	render() {
+		return (
+			<div>start</div>
+		);
+	}
+}
+
+export default App;
